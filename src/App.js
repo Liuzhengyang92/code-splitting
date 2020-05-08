d1306d68b66134d781de38700f32f4cf5b8bb2db
felixatchur <felix@churinc.com>
2020-05-08 22:43:52 +1000

    use high order function and async component to do code splitting

--- src/App.js
+++ src/App.js
@@ -2,48 +2,50 @@ import React, { Component } from 'react';
 import Page1 from './components/Page1';
 import './App.css';
 
+import AsyncComponent from './components/AsyncComponent';
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
       route: 'page1',
-      component: ''
+      component: null
     };
   }
 
   onRouteChange = (route) => {
-    // No Code Splitting
-    // this.setState({ route });
+    // high order function code splitting
+    this.setState({ route });
     // With Code Splitting:
-    if (route === 'page1') {
-      this.setState({ route: route });
-    } else if (route === 'page2') {
-      import('./components/Page2').then((Page2) => {
-        console.log(Page2);
-        this.setState({ route: route, component: Page2.default });
-      });
-    } else if (route === 'page3') {
-      import('./components/Page3').then((Page3) => {
-        console.log(Page3);
-        this.setState({ route: route, component: Page3.default });
-      });
-    }
+    // if (route === 'page1') {
+    //   this.setState({ route: route });
+    // } else if (route === 'page2') {
+    //   import('./components/Page2').then((Page2) => {
+    //     this.setState({ route: route, component: Page2.default });
+    //   });
+    // } else if (route === 'page3') {
+    //   import('./components/Page3').then((Page3) => {
+    //     this.setState({ route: route, component: Page3.default });
+    //   });
+    // }
   }
 
   render() {
-    // const { route } = this.state;
-    // if (route === 'page1') {
-    //   return <Page1 onRouteChange={this.onRouteChange} />;
-    // } else if (route === 'page2') {
-    //   return <Page2 onRouteChange={this.onRouteChange} />;
-    // } else if (route === 'page3') {
-    //   return <Page3 onRouteChange={this.onRouteChange} />;
-    // }
-    if (this.state.route === 'page1') {
+    const { route } = this.state;
+    if (route === 'page1') {
       return <Page1 onRouteChange={this.onRouteChange} />;
-    } else {
-      return <this.state.component onRouteChange={this.onRouteChange} />;
+    } else if (route === 'page2') {
+      const AsyncPage2 = AsyncComponent(() => import('./components/Page2'));
+      return <AsyncPage2 onRouteChange={this.onRouteChange} />;
+    } else if (route === 'page3') {
+      const AsyncPage3 = AsyncComponent(() => import('./components/Page3'));
+      return <AsyncPage3 onRouteChange={this.onRouteChange} />;
     }
+    // if (this.state.route === 'page1') {
+    //   return <Page1 onRouteChange={this.onRouteChange} />;
+    // } else {
+    //   return <this.state.component onRouteChange={this.onRouteChange} />;
+    // }
   }
 }
 
